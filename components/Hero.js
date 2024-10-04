d7cd7e89ed920d2615cb975985fe1a32dae96a7a
Keri West <keriwest88@gmail.com>
2024-10-04 15:36:59 -0400

    added hero to page

---
+++ components/Hero.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <header className="hero">
+            <img className="heroImage" src="../images/photo-grid.png" />
+            
+            <h1>Online Experiences</h1>
+                <p>
+                    Join unique interactive activities led by one-of-a-kind hosts—all without leaving home. 
+                </p>
+        </header>
+    )
+}
