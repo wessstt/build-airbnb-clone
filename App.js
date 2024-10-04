300c07e7f72be8b4502934281cdad94f39fb88bd
Keri West <keriwest88@gmail.com>
2024-10-04 15:23:30 -0400

    added navbar

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Navbar from "./components/Navbar.jsx"
+
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            
+        </>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+   return ( 
+        <nav className="navbar">
+            <img className="navLogo" src="../images/airbnb-logo.png" />
+        </nav>
+    )
+}
