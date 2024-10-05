9e29eb5aceb56a01d9b9b6c5df0f47919abce6ea
Keri West <107593956+wessstt@users.noreply.github.com>
2024-10-05 18:41:45 -0400

    Delete src directory

--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Navbar() {
-   return ( 
-        <nav>
-            <img className=".nav--logo" src="../images/airbnb-logo.png" alt="AirBnb logo" />
-        </nav>
-    )
-}
